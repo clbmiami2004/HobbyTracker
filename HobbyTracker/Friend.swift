da01ee499515bc91c09dd73bde0344b79290b27e
Christian Lorenzo <clbmiami2004@gmail.com>
2019-09-25 18:48:58 -0700

    Added friends model to project.

---
+++ HobbyTracker/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  HobbyTracker
+//
+//  Created by Christian Lorenzo on 9/25/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import Foundation
+
+struct Friend {
+    let name: String
+    let hhometown: String
+    var hobbies: [String]
+}
---
+++ HobbyTracker/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  HobbyTracker
+//
+//  Created by Christian Lorenzo on 9/25/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
